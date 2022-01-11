6a4896174f9f59f59da986f5c63edd5c347ef75e
ZobaerIslam <zobaerislam62@gmail.com>
2022-01-12 04:57:46 +0600

    built responsive navigation section

---
+++ js/script.js
@@ -0,0 +1,9 @@
+let navbar = document.querySelector('.header .nav__container .navigation__bar');
+
+document.querySelector('#nav-icon').onclick = () => {
+    navbar.classList.toggle('active');
+}
+
+document.querySelector('#nav-icon2').onclick = () => {
+    navbar.classList.toggle('active');
+}
